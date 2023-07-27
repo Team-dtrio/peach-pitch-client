b3221397677b115e0a4b9607a046335ed7a7b3ab
문형석 <hyn9xc@gmail.com>
2023-07-27 13:50:58 +0900

    replaced spaghetti code by using attrs module

--- src/components/Object/index.jsx
+++ src/components/Object/index.jsx
@@ -1,87 +1,48 @@
 import { styled } from "styled-components";
 
-function Object({ spec, pointedObject, onObjectClick, onObjectMouseDown }) {
-  switch (spec.type) {
-    case "Textbox":
-      return (
-        <Textbox
-          spec={spec}
-          pointedObject={pointedObject}
-          onClick={() => onObjectClick(spec)}
-          onMouseDown={onObjectMouseDown}
-        />
-      );
-    case "Square":
-      return (
-        <Square
-          spec={spec}
-          pointedObject={pointedObject}
-          onClick={() => onObjectClick(spec)}
-          onMouseDown={onObjectMouseDown}
-        />
-      );
-    case "Triangle":
-      return (
-        <Triangle
-          spec={spec}
-          pointedObject={pointedObject}
-          onClick={() => onObjectClick(spec)}
-          onMouseDown={onObjectMouseDown}
-        />
-      );
-    case "Circle":
-      return (
-        <Circle
-          id="object"
-          className="object"
-          spec={spec}
-          pointedObject={pointedObject}
-          onClick={() => onObjectClick(spec)}
-          onMouseDown={() => onObjectMouseDown(spec)}
-        />
-      );
-    default:
-      return <div />;
-  }
-}
-
-const Textbox = styled.input`
+const BaseComponent = styled.div`
   position: absolute;
-  left: 50%;
-  right: 50%;
-  width: 100px;
-  height: 100px;
-  border: 1px dashed #222;
   cursor: pointer;
-`;
-const Square = styled.div`
-  position: absolute;
-  left: ${({ spec }) => spec.x}px;
-  top: ${({ spec }) => spec.y}px;
+  left: ${({ spec, pointedObject }) =>
+    spec?._id === pointedObject?._id ? pointedObject.x : spec.x}px;
+  top: ${({ spec, pointedObject }) =>
+    spec?._id === pointedObject?._id ? pointedObject.y : spec.y}px;
   width: ${({ spec }) => spec.width}px;
   height: ${({ spec }) => spec.height}px;
-  background-color: #222;
-  cursor: pointer;
+  background-color: ${({ spec }) => spec.color || "#222"};
 `;
-const Triangle = styled.div`
-  position: absolute;
-  left: ${({ spec }) => spec.x}px;
-  top: ${({ spec }) => spec.y}px;
-  height: ${({ spec }) => spec.height}px;
+const TextBox = styled(BaseComponent).attrs({ as: "input" })``;
+const Image = styled(BaseComponent).attrs({ as: "img" })``;
+const Square = styled(BaseComponent)``;
+const Triangle = styled(BaseComponent)`
+  width: 0;
+  height: 0;
   border-left: 50px solid transparent;
   border-right: 50px solid transparent;
   border-bottom: 50px solid green;
-  cursor: pointer;
 `;
-const Circle = styled.div`
-  position: absolute;
-  left: ${({ spec }) => spec.x}px;
-  top: ${({ spec }) => spec.y}px;
-  width: ${({ spec }) => spec.width}px;
-  height: ${({ spec }) => spec.height}px;
+const Circle = styled(BaseComponent)`
   border-radius: 100%;
-  background-color: #222;
-  cursor: pointer;
 `;
+
+function Object({ spec, pointedObject, onObjectClick, onObjectMouseDown }) {
+  const components = {
+    TextBox,
+    Image,
+    Square,
+    Triangle,
+    Circle,
+  };
+  const Tag = components[spec.type] || "div";
+
+  return (
+    <Tag
+      spec={spec}
+      pointedObject={pointedObject}
+      onClick={() => onObjectClick(spec)}
+      onMouseDown={onObjectMouseDown}
+    />
+  );
+}
 
 export default Object;
